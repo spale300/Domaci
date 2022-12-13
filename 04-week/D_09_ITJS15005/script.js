3f446583a962b71fa179f32396ffd2bffe154f82
spale300 <spalecar1@hotmail.com>
2022-12-13 14:38:07 +0100

    d9

---
+++ 04-week/D_09_ITJS15005/script.js
@@ -0,0 +1,32 @@
+//01. Dodati na stranicu n puta jednu siku. Slikama koje su na parnim pozicijama postavljati crveni okvir, slikama koje su na neparnim pozicijama postavljati plavi okvir.
+
+let n = 11;
+
+for (i = 1; i <= n; i++) {
+  if (i % 2 == 0) {
+    document.write(
+      `<img src='slika.png' style="border:5px solid red; margin: 20px">`
+    );
+  } else {
+    document.write(
+      `<img src='slika.png' style="border:5px solid blue;margin: 20px">`
+    );
+  }
+}
+
+//02.For petljom odrediti da li je dati prirodan broj n prost.
+
+n = 24;
+let brojDelioca = 0;
+
+for (i = 1; i <= n; i++) {
+  if ((n / 2) % i == 0) {
+    brojDelioca++;
+  }
+}
+
+if (brojDelioca > 2) {
+  console.log(`Broj ${n} je slozen broj.`);
+} else {
+  console.log(`Broj ${n} je prost broj.`);
+}
